14aa9a75f160fc4713ddc752da4dec5d77dd3ebd
Harrison <harrisontran@outlook.com>
2021-08-27 21:48:34 -0700

    list and post method done

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -12,6 +12,63 @@ function list(req, res, next) {
   res.json({ data: dishes });
 }
 
+function validateDish(req, res, next) {
+  const { data: { name, description, price, image_url } = {} } = req.body;
+  if (
+    name &&
+    description &&
+    price &&
+    Number.isInteger(price) &&
+    price > 0 &&
+    image_url
+  ) {
+    return next();
+  }
+
+  if (name === undefined || name === "")
+    return next({
+      status: 400,
+      message: "name",
+    });
+  if (description === undefined || description === "")
+    return next({
+      status: 400,
+      message: "description",
+    });
+  if (price === undefined || price <= 0)
+    return next({
+      status: 400,
+      message: "price",
+    });
+
+  if (image_url === undefined || image_url === "")
+    return next({
+      status: 400,
+      message: "image_url",
+    });
+}
+
+function create(req, res, next) {
+  // create the data from dishes with the new id
+  // attach the data to req.body
+  // push the new dish to the data
+  const { data: { name, description, price, image_url } = {} } = req.body;
+  const newDish = {
+    id: nextId(),
+    name,
+    description,
+    price,
+    image_url,
+  };
+
+  dishes.push(newDish);
+  console.log("newDish ", newDish);
+  console.log("Dishes ", dishes);
+
+  res.status(201).json({ data: newDish });
+}
+
 module.exports = {
   list,
+  create: [validateDish, create],
 };
--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -2,7 +2,14 @@ const router = require("express").Router();
 
 // TODO: Implement the /dishes routes needed to make the tests pass
 const controller = require("./dishes.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/").get(controller.list);
+// router.route("/:dishId").get(controller.read).all(methodNotAllowed);
+
+router
+  .route("/")
+  .get(controller.list)
+  .post(controller.create)
+  .all(methodNotAllowed);
 
 module.exports = router;
